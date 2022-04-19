dffe3b1b4ff3e36aea9b65096f9c58545f8644c4
Paulo Santos <pauloed_@outlook.com>
2022-04-18 21:07:29 -0300

    Select alias e order by

---
+++ DML/selectAlias.sql
@@ -0,0 +1,5 @@
+/* Podemos renomear o nome das colunas com um alias ao realizar um select */
+
+SELECT nome_coluna  nome_alias_coluna , nome_coluna nome_alias_coluna FROM nome_tabela WHERE condição = valor
+
+SELECT nome_coluna AS nome_alias_coluna , nome_coluna AS nome_alias_coluna FROM nome_tabela WHERE condição = valor
---
+++ DML/selectOrderBy.sql
@@ -0,0 +1,9 @@
+/* É usado para ordenar os dados que serão retornados pela consulta*/
+
+/* O padrão é em ordem crescente */
+
+SELECT * FROM nome_tabela ORDER BY valor;
+
+SELECT * FROM nome_tabela ORDER BY valor DESC;
+
+SELECT * FROM nome_tabela ORDER BY valor ASC;
